6663b80b0f6d2b034be4b2a77acb5002ec53b8ba
sharad239 <sharadsonilko@gmail.com>
2019-08-19 14:08:39 +0530

    update bg images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,9 +15,9 @@ $(window).ready(function() {
 	'use strict';
 	$.vegas('slideshow', {
 	  backgrounds:[
-		{ src:'../assets/images/bg-slider/bg-1.jpg', fade:1000 },
-		{ src:'../assets/images/bg-slider/bg-2.jpg', fade:1000 },
-		{ src:'../assets/images/bg-slider/bg-3.jpg', fade:1000 }
+		{ src:'../website/assets/images/bg-slider/bg-1.jpg', fade:1000 },
+		{ src:'../website/assets/images/bg-slider/bg-2.jpg', fade:1000 },
+		{ src:'../website/assets/images/bg-slider/bg-3.jpg', fade:1000 }
 	  ]
 	})();
 });
